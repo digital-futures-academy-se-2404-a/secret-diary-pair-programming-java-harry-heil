e6d887fd308424023c3566076bd0569dd27a632b
Rustic-Citrus <mrcurtis.english@gmail.com>
2024-05-08 11:11:06 +0100

    test: added first Diary.addEntry() test

---
+++ src/main/java/org/digitalfuturesacademy/Entry.java
@@ -0,0 +1,11 @@
+package org.digitalfuturesacademy;
+
+public class Entry {
+  private String text;
+  private String date;
+
+  public Entry(String text, String date) {
+    this.text = text;
+    this.date = date;
+  }
+}
--- src/test/java/org/digitalfuturesacademy/DiaryTest.java
+++ src/test/java/org/digitalfuturesacademy/DiaryTest.java
@@ -1,4 +1,32 @@
 package org.digitalfuturesacademy;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
 public class DiaryTest {
+  private Diary testDiary;
+  private Entry testEntry;
+
+  @BeforeEach
+  public void testSetup() {
+    testDiary = new Diary();
+    testEntry = mock(Entry.class);
+  }
+
+  @Nested
+  @DisplayName("Diary.addEntry() Test Suite")
+  public class AddEntryTests {
+    @Test
+    @DisplayName("It should throw an exception when a user tries to add an entry to a locked diary.")
+    public void testDoesNotAddEntryToLockedDiary() {
+      RuntimeException thrownException = assertThrows(RuntimeException.class, () -> testDiary.addEntry(testEntry));
+
+      assertEquals("Diary is locked", thrownException.getMessage());
+    }
+  }
 }
